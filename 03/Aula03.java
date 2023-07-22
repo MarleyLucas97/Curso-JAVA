6061439105339ec9e3ed1e91d15e0c88b943f561
Marley De Lucas <marleydelucas97@gmail.com>
2023-07-22 12:29:27 -0300

    Update Aula03.java

--- 03/Aula03.java
+++ 03/Aula03.java
@@ -5,8 +5,10 @@ public class Aula03{
         //Aciona a tomada de decisao quando cumpre os requisitos de uma expressao logica
         //Expressoes logicas: ==  >  <  >=  <=  !=  !   (True / False)
 
-        int nota = 80, media = 60, faltas = 10, maxfaltas = 5;
-        int res;
+        /*int nota = 80, media = 60, faltas = 10, maxfaltas = 5;*/
+        /*String res; */
+        /*int res;*/
+        int pos = 7;
 
 
         /*if(nota>=media && faltas<=maxfaltas){
@@ -24,7 +26,23 @@ public class Aula03{
         /*res=(nota>=media ? 0 : 1);
         System.out.println("Resultado: " + (res==1?"Aprovado":"Reprovado"));*/
 
-        
+        switch (pos) {
+            case 1:
+                System.out.println("Primeiro lugar.");
+                break;
+            case 2:
+                System.out.println("Segundo lugar.");
+                break;
+            case 3:
+                System.out.println("Terceiro lugar.");
+                break;
+            case 4: case 5: case 6:
+                System.out.println("Ganhou um premio de participacao.");
+                break;
+            default:
+                System.out.println("Nao ganhou premio.");
+                break;
+        }
 
         //Tabela da verdade para AND &&
         /*
