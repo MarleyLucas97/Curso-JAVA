03ce6031383a6409b2d0a156b4dfaca2f2418ab5
Marley De Lucas <marleydelucas97@gmail.com>
2023-07-29 09:43:28 -0300

    Aula10

---
+++ 10 - Classes em Java/Aula10.java
@@ -0,0 +1,22 @@
+//tudo em Java sao classes, a classe se torna princial pelo metodo main
+//toda classe se inicia por *modificador de acesso, a indicacao de que e 
+//uma classe e o *Nome da classe
+public class Aula10{
+    //private e public
+    //modificadores de acesso
+    //metodos e atributos definem o objeto da classe
+    public static void main(String[] args) {
+        //esse e um processo de instanciacao da classe Aula10
+        //O j1 esta recebendo um novo objeto da classe Aula10
+        //cada objeto e individual
+        
+        Aula10a j1 = new Aula10a(1);
+        Aula10a j2 = new Aula10a(2);
+        Aula10a j3 = new Aula10a(3);
+
+        int num=0;
+        //num paralelo com inteiros se percebe a semelhanca
+        //recebe *tipo,*nome e *valor atribuido
+        //heap - memoria dinamica
+    }
+}
---
+++ 10 - Classes em Java/Aula10a.java
@@ -0,0 +1,10 @@
+public class Aula10a {
+    //metodo construtor: e executado quando um objeto dessa 
+    //classe for instanciado
+    //metodo construtor nao tem retorno, nao e necessario especificar 
+    //o tipo
+    //precisa ter o mesmo nome da classe.
+    public Aula10a(int n){
+        System.out.printf("%nJogador numero %d criado%n",n);
+    }
+}
