55a5b1ab39502ab6950754c2fa748b98375b6f56
Marley De Lucas <marleydelucas97@gmail.com>
2024-01-23 01:17:27 -0300

    Create Aula36.java
    lol

---
+++ 36 - Array List/Aula36.java
@@ -0,0 +1,20 @@
+// "Confie no SENHOR de todo coração, não dependa do seu próprio entendimento." -Provérbios, 3 : 5
+// "Deus abençoe este codigo e quem vier a lê-lo." -Marley de Lucas, 22/01/2024 10:28
+
+import java.util.ArrayList;
+
+public class Aula36 {
+    public static void main(String[] args) {
+        
+        ArrayList<Integer> numeros = new ArrayList<>();
+        ArrayList<String> carros = new ArrayList<>();
+
+        numeros.add(11);
+        nuemros.add(22);
+        numeros.add(33);
+
+        for(int e:numeros){
+            System.out.print(e);
+        }
+    }
+}
